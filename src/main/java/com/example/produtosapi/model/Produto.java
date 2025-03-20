26ad4765821318f52d4d60644ed6837c34958b95
Arthur Fernandes Vaz <155276471+arthurvaz18@users.noreply.github.com>
2025-03-20 16:23:45 -0300

    Adicionando O ProdutoRepository e Configurando o save no Controller

--- src/main/java/com/example/produtosapi/model/Produto.java
+++ src/main/java/com/example/produtosapi/model/Produto.java
@@ -1,11 +1,25 @@
 package com.example.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
-
+    @Id
+    @Column(name ="id")
     private String id;
+
+    @Column
     private String nome;
+
+    @Id
     private String descricao;
+
+    @Column
     private Double preco;
 
     public String getId() {
